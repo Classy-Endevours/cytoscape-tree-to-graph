650c3ecabb9f12efff3eab8c65f9f8f04c2f7730
Piyush Dubey <piyush115dubey@gmail.com>
2022-12-06 13:16:04 +0530

    Added algorithm

--- src/graph-theory/graph/graph.ts
+++ src/graph-theory/graph/graph.ts
@@ -1,34 +1,89 @@
-import cytoscape, {
-  Core,
-} from 'cytoscape'
-import { Parser  } from '../parser'
-import { Node } from '../tree/tree.interface'
-import { IEdge, IGraph, IVertex, Visit } from './graph.interface'
+import cytoscape, { Core } from "cytoscape";
+import { Parser } from "../parser";
+import { Node } from "../tree/tree.interface";
+import { IEdge, IGraph, IVertex, Visit } from "./graph.interface";
 
 /**
  * (1) Implement IGraph interface
  */
 export class Graph {
-  cy: Core
+  cy: Core;
+  _rootId: string;
+
   constructor(tree: Node) {
     /**
      * (2) Use Parser interface to parse tree
      */
-
+    const parsedTree = Parser(tree);
     /**
      * (3) Initialize cytoscape with parsed data
      */
-    const parsedTree = Parser(tree)
-    this.cy = cytoscape()
+    this.cy = cytoscape({
+      elements: {
+        nodes: parsedTree.vertices.map((e) => ({
+          data: {
+            id: e.id,
+            name: e.name,
+          },
+        })),
+        edges: parsedTree.edges.map((e) => ({
+          data: {
+            source: e.source,
+            target: e.target,
+          },
+        })),
+      },
+    });
+    /**
+     * (4) Setting id of the root node
+     * Since couldn't find any way to get the first root node in cytoscape
+     */
+    this._rootId = tree.id
+  }
+
+  get rootId(): typeof this._rootId {
+    return `#${this._rootId}`
   }
 
   /**
    * (4) Use cytoscape under the hood
    */
-  bfs(visit: Visit<IVertex, IEdge>) {}
+  bfs(visit: Visit<IVertex, IEdge>) {
+    this.cy.elements().bfs({
+      roots: this.rootId,
+      visit: function(v, e, u, i, depth){
+        const vertex: IVertex = {
+          name: v.data().name,
+          id: v.id(),
+        } 
+        const firstEdge = v.connectedEdges().first()
+        const edge: IEdge = {
+          target: firstEdge.target().id(),
+          source: firstEdge.source().id(),
+        } 
+        visit(vertex, edge)
+      }
+    })
+  }
 
   /**
    * (5) Use cytoscape under the hood
    */
-  dfs(visit: Visit<IVertex, IEdge>) {}
+  dfs(visit: Visit<IVertex, IEdge>) {
+    this.cy.elements().dfs({
+      roots: this.rootId,
+      visit: function(v, e, u, i, depth){
+        const vertex: IVertex = {
+          name: v.data().name,
+          id: v.id(),
+        } 
+        const firstEdge = v.connectedEdges().first()
+        const edge: IEdge = {
+          target: firstEdge.target().id(),
+          source: firstEdge.source().id(),
+        } 
+        visit(vertex, edge)
+      }
+    })
+  }
 }
--- src/graph-theory/parser.ts
+++ src/graph-theory/parser.ts
@@ -2,9 +2,38 @@ import { GraphData, IEdge, IVertex } from './graph/graph.interface'
 import { Node, Tree } from './tree/tree.interface'
 
 export const Parser = (tree: Node, graph?: GraphData, parent?: Node): GraphData => {
-    return {
-        vertices: [],
-        edges: []
+    const root: IVertex = {
+        id: tree.id,
+        name: tree.name
     }
+
+    const vertices: IVertex[] = []
+    const edges: IEdge[] = []
+    vertices.push(root)
+
+    for(const child of tree.children){
+        const edge: IEdge = {
+            source: tree.id,
+            target: child.id
+        }
+        edges.push(edge)
+    }
+    let currentGraph: GraphData
+    if(!graph) {
+        currentGraph = {
+            vertices,
+            edges
+        }
+    } else {
+        currentGraph = graph
+        currentGraph.edges.push(...edges)
+        currentGraph.vertices.push(...vertices)
+    }
+
+    for(const child of tree.children){
+        Parser(child, currentGraph)
+    }
+
+    return currentGraph
 }
 
